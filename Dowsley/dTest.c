fecf822b14564940bab0a1820044675e5e69ee9c
João Dowsley <joao.dowsley850@gmail.com>
2019-10-10 23:57:25 -0300

    Oi

--- Dowsley/dTest.c
+++ Dowsley/dTest.c
@@ -24,34 +24,7 @@ void gotoxy(int x,int y)
 
 int main()
 {
-	char middlefinger[] = "        _____\n       |    ||\n       |\\___/|\n       |     |\n       |     |\n       |     |\n       |     |\n       |     |\n       |     |\n   ____||____|____\n  /    |     |     \\\n /     |     |    | \\\n|      |     |    |  |\n|      |     |    |  |\n|                 |  |\n|                 |  |\n|                    /\n|                   /\n \\                 /\n  \\               /\n   |             |\n   |             |\n";
-	int size = sizeof (middlefinger) / sizeof(char);
+	
 
-	char c;
-	int y;
-	int x_padrao = 0;
-	int x;
-
-	system("cls");
-	for (int i = 0; i < 69; i++)
-	{
-		x = x_padrao;
-		y = 0;
-	    for (int j = 0; j < size; j++)
-	    {
-	    	c = middlefinger[j];
-
-	    	if (c == '\n'){
-	    		y++;
-	    		x = x_padrao;
-	    	}
-
-	        gotoxy(x,y);
-	        printf("%c", c);
-	        Sleep(1);
-	        x++;
-	    }
-	    x_padrao += 23;
-	}
 	return 0;
 }
