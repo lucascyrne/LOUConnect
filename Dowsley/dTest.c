31cf9d6ef9d5bf53f3d556c9164f34305cfd42c2
Dowsley <joao.dowsley850@gmail.com>
2019-10-11 23:41:17 -0300

    Implementando primeira BT

--- Dowsley/dTest.c
+++ Dowsley/dTest.c
@@ -21,10 +21,101 @@ void gotoxy(int x,int y)
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
 }
 
+typedef struct user{
+	char nome[50];
+	char email[40];
+	char cargo[40];
+	char cpf[11];
+	char telefone[11];
+	char tags[100];
+	int idade;
+
+	struct user* esquerda;
+	struct user* direita;
+}USER;
+
+void inserirNo(USER**);
+void exibirPre(USER*);
+void exibirIn(USER*);
+void exibirPos(USER*);
 
 int main()
 {
+	long int recsize = sizeof(e);	// Tamanho de cada struct user.
+	FILE *fp, *ft;	// ponteiros pra arquivo
 	
+	USER* arvore = NULL;	// Inicia arvore.
+
+    fp = fopen("Users.DAT","rb+");
+    if(fp == NULL)
+    {
+        fp = fopen("Users.DAT","wb+");
+        if(fp == NULL)
+        {
+            printf("Impossivel abrir arquivo.");
+            exit(1);
+        }
+    }
+
+
+
+
+
+
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++)
+	{
+		scanf("%d", &n);
+		inserirNo(&arvore, n);
+	}
 
 	return 0;
+}
+
+void inserirNo(USER** arvore)
+{
+	USER* novo_user = (USER*)malloc(sizeof(USER));
+	novo_user->valor = n;
+	novo_user->esquerda = NULL;
+	novo_user->direita = NULL;
+
+	if (*arvore == NULL)
+        *arvore = novo_user;
+    else
+    {
+        if (n <= (*arvore)->valor)
+            inserirNo(&(*arvore)->esquerda, n);
+        else
+        	inserirNo(&(*arvore)->direita, n);
+    }
+}
+
+void exibirPre(USER* arvore)
+{
+	if (arvore != NULL)
+	{
+		printf(" %d", arvore->valor);
+		exibirPre(arvore->esquerda);
+		exibirPre(arvore->direita);
+	}
+}
+
+void exibirIn(USER* arvore)
+{
+	if (arvore != NULL)
+	{
+		exibirIn(arvore->esquerda);
+		printf(" %d", arvore->valor);
+		exibirIn(arvore->direita);
+	}
+}
+
+void exibirPos(USER* arvore)
+{
+	if (arvore != NULL)
+	{
+		exibirPos(arvore->esquerda);
+		exibirPos(arvore->direita);
+		printf(" %d", arvore->valor);
+	}
 }
