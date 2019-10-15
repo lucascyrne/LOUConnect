13623d455687b54b2319ed85eedb26339e17682d
João Dowsley <joao.dowsley850@gmail.com>
2019-10-14 21:27:18 -0300

    BST Funcionando com nomes. Features: Adicionar, listar, alterar, interface simples. Pretendo colocar a feature de remover e mais tags, alem de serializar a arvore em arquivo.

--- Dowsley/dTest.c
+++ Dowsley/dTest.c
@@ -1,13 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>   // Para gerenciamento de memória
-#include <conio.h>   // para gerenciamento de input e output direto do console.
-#include <windows.h> // Para funções relacionadas ao windows
-#include <string.h>  // Operações de string
-#include <unistd.h>  // Para sleep functions
-#define NULO -1  // Valor que sinaliza NULL na des-serialização, e por conveniência fica guardado na idade.
+#include <stdio.h> ///for input output functions like printf, scanf
+#include <stdlib.h>
+#include <conio.h>
+#include <windows.h> ///for windows related functions (not important)
+#include <string.h>  ///string operations
 
-COORD coord = {0,0}; /// top-left corner of window
+typedef struct USER{
+	char nome[10];
+	struct USER* esquerda;
+	struct USER* direita;
+}USER;
 
+COORD coord = {0,0};
 void gotoxy(int x,int y)
 {
     coord.X = x;
@@ -15,123 +18,117 @@ void gotoxy(int x,int y)
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
 }
 
-typedef struct USER{
-	char nome[10];
-
-	struct USER* esquerda;
-	struct USER* direita;
-}USER;
-
-void inserirNo(USER**, USER*);
+void inserirNo(USER**, char*);
 void exibirPre(USER*);
 void exibirIn(USER*);
 void exibirPos(USER*);
+USER* buscarNo(USER*, char*);
 
 int main()
 {
-	FILE *fp, *ft;	// ponteiros pra arquivo
-	USER* arvore = NULL;  // Inicia arvore.
-	
-	USER u;	// Vessel para novo usuário.
-	long int recsize = sizeof(u);  // Tamanho de cada struct user.
+	USER* arvore = NULL;  // Criação da Árvore
+	char nome[10], alter[10];   // Nome q entrará na árvore.
+	char another, choice;
 
-	char choice, another;
-
-
-	/******* Criação/Loading do arquivo. *****
-    fp = fopen("Users.DAT","rb+");
-    if(fp == NULL)
+    while(1)
     {
-        fp = fopen("Users.DAT","wb+");
-        if(fp == NULL)
-        {
-            printf("Impossivel abrir arquivo.");
-            exit(1);
-        }
-        printf("Arquivo criado.\n");
-    }
-    else 
-    	printf("Arquivo aberto.\n");*/
-
-
-    /****** Des-serialização da árvore. *****
-    printf("** Carregando árvore... **\n");
-
-	rewind(fp);  // Move o cursor para o começo do arquivo
-	while(fread(&e,recsize,1,fp)==1)  // lê o arquivo e resgata record vez por vez, avançando cursor.
-    {
-    	if (e.idade != NULO)  //  Se a idade for -1 o struct representa o valor nulo.
-    	{
-    		inserirNo(&arvore, &e);  // Insere na árvore o user carregado
-    	}
-
-    	else
-    }
-    printf("** Árvore carregada. ***");*/
-
-	while(1)
-	{
-		system("cls");  // Limpa a janela do console
-        gotoxy(30,10);
-        printf("1. Adicionar Registro");
+        system("cls"); ///clear the console window
+        gotoxy(30,10); /// move the cursor to postion 30, 10 from top-left corner
+        printf("1. Adicionar Registro"); /// option for add record
         gotoxy(30,12);
-        printf("2. Listar Registros");
+        printf("2. Listar Registros"); /// option for showing existing record
         gotoxy(30,14);
-        printf("3. Modificar Registros");
+        printf("3. Modificar Registros"); /// option for editing record
         gotoxy(30,16);
-        printf("4. Deletar Registros");
+        printf("4. Deletar Registros"); /// option for deleting record
         gotoxy(30,18);
-        printf("5. Fechar Programa");  // Serialização acontece no case 5.
+        printf("5. Sair"); /// exit from the program
         gotoxy(30,20);
-        printf("Sua escolha: ");
-        fflush(stdin);  // limpa o input buffer
-        choice  = getche();  // recebe input do teclado
+        printf("Escolha: "); /// enter the choice 1, 2, 3, 4, 5
         
+        fflush(stdin); /// flush the input buffer
+        choice  = getche(); /// get the input from keyboard
         switch(choice)
         {
-	        case '1':
+	        case '1':  // Se o usuário pressionar '1': Adiciona registro.
 	            system("cls");
-	            another = 'y';
-	            
-	            while(another == 'y')  /// if user want to add another record
+	            another = 's';
+	            while(another == 's')  /// if user want to add another record
 	            {
-	                printf("\nInsira o nome: ");
-	                scanf("%s",u.nome);
+	                printf("\nInsira o nome que deseja adicionar: ");
+	                scanf("%s",nome);
 
-	                u.esquerda = NULL;
-					u.direita = NULL;
+					inserirNo(&arvore, nome);
 
-	                inserirNo(&arvore, &u);
-	          
-	                printf("\nAdd another record(y/n) ");
+	                printf("\nAdicionar outro registro? (s/n) ");
 	                fflush(stdin);
 	                another = getche();
 	            }
 	            break;
-        
-
-	        case '2':
+	        case '2':  // Exibe Registros
 	            system("cls");
-	            exibirIn(arvore);
+
+				printf("\nPre.:");
+				exibirPre(arvore);
+
+				printf("\nIn..:");
+				exibirIn(arvore);
+
+				printf("\nPost:");
+				exibirPos(arvore);
+
 	            getch();
 	            break;
+
+	        case '3':  // Modifica registros existentes
+	            system("cls");
+	            another = 's';
+	            while(another == 's')
+	            {
+	               	printf("\nInsira o nome que deseja alterar: ");
+	                scanf("%s",nome);
+	                printf("\nDigite o novo nome: ");
+	                scanf("%s",alter);
+
+	                strcpy(buscarNo(arvore, nome)->nome, alter);
+
+	                printf("\nModificar outro registro? (s/n)");
+	                fflush(stdin);
+	                another = getche();
+	            }
+	            break;
+	        case '4':
+	            system("cls");
+	            another = 's';
+	            while(another == 's')
+	            {
+	               	printf("\nEm breve...");
+	                printf("\nDeletar outro registro? (s/n)");
+	                fflush(stdin);
+	                another = getche();
+	            }
+	            break;
+	        case '5':
+	            return 0; /// exit from the program
         }
 	}
-
-	return 0;
 }
 
-
-void inserirNo(USER** arvore, USER* novoUser)
+void inserirNo(USER** arvore, char* nome)
 {
+	USER* novoUser = (USER*)malloc(sizeof(USER));
+	strcpy(novoUser->nome, nome);
+	novoUser->esquerda = NULL;
+	novoUser->direita = NULL;
+
 	if (*arvore == NULL)
         *arvore = novoUser;
     else
     {
         if (strcmp(novoUser->nome, (*arvore)->nome) <= 0)
-            inserirNo(&(*arvore)->esquerda, novoUser);
+            inserirNo(&(*arvore)->esquerda, nome);
         else
-        	inserirNo(&(*arvore)->direita, novoUser);
+        	inserirNo(&(*arvore)->direita, nome);
     }
 }
 
@@ -139,7 +136,7 @@ void exibirPre(USER* arvore)
 {
 	if (arvore != NULL)
 	{
-		printf("\nNome: %s", arvore->nome);
+		printf(" %s", arvore->nome);
 		exibirPre(arvore->esquerda);
 		exibirPre(arvore->direita);
 	}
@@ -150,7 +147,7 @@ void exibirIn(USER* arvore)
 	if (arvore != NULL)
 	{
 		exibirIn(arvore->esquerda);
-		printf("\nNome: %s", arvore->nome);
+		printf(" %s", arvore->nome);
 		exibirIn(arvore->direita);
 	}
 }
@@ -161,6 +158,19 @@ void exibirPos(USER* arvore)
 	{
 		exibirPos(arvore->esquerda);
 		exibirPos(arvore->direita);
-		printf("\nNome: %s", arvore->nome);
+		printf(" %s", arvore->nome);
 	}
+}
+
+USER* buscarNo(USER* raiz, char* nome)
+{
+	if (raiz == NULL || strcmp(raiz->nome, nome) == 0); 
+    	return raiz; 
+     
+    // Key is greater than root's key 
+    if (strcmp(nome, raiz->nome) > 0) 
+       return buscarNo(raiz->direita, nome); 
+  
+    // Key is smaller than root's key 
+    return buscarNo(raiz->esquerda, nome); 
 }
